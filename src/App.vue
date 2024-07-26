edac8c5a4ae49df5fe9c27e5a6d6d6fe79855227
Jens Laufer <jens.laufer@solytics.de>
2024-07-26 15:34:13 +0200

    Initial

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col w-screen h-screen bg-blue-100 font-poppins">
+    <router-view class="grow pt-20" />
+    <div class="sticky bottom-0 bg-slate-200 text-black text-xs p-4">
+      <p>&copy; {{ year }} Solytics GmbH</p>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const year = ref(new Date().getFullYear());
+</script>
---
+++ src/views/LandingPage1.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col mx-auto items-center">
+    <h1 class="text-4xl font-bold text-center w-8/12">Attract high-quality, low-cost leads with Software
+      tools and
+      Quizzes</h1>
+    <registration class="mt-20 w-5/12 p-8" title="Wanna learn more?" button-label="Book a consultancy call" />
+  </div>
+</template>
+<script setup>
+import Registration from './Registration.vue';
+</script>
