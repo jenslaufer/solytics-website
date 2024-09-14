ffa6cfa786905b57751b9836c04641603d379908
Jens Laufer <jens.laufer@solytics.de>
2024-09-14 07:24:49 +0200

    moved on

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col w-screen h-screen font-poppins">
-    <router-view class="grow pt-20" />
-    <div class="sticky bottom-0 flex gap-3 bg-slate-100 text-slate-600 text-xs italic p-3">
+    <router-view class="grow" />
+    <div class="sticky flex gap-4 bg-slate-100 text-slate-600 text-xs italic p-3">
       <p>&copy; {{ year }} Solytics GmbH</p>
       <a href="" class="hover:text-black">Impressum</a>
     </div>
---
+++ src/views/ToolFunnel.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col bg-blue-200 items-center pt-10">
+    <h1 class="font-bold text-center pb-2 text-3xl">Business Owner?</h1>
+    <h2 class="text-lg p-2 font-semibold text-center">Attract high-quality, low-cost leads with Software Tools and
+      Quizzes</h2>
+    <registration class="mt-8" button-label=" Book a consultancy call" />
+  </div>
+</template>
+<script setup>
+import Registration from './Registration.vue';
+</script>
