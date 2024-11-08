1825ae962d4ee0bd1f2a4044cd1f95022a3b62f6
Jens Laufer <jens.laufer@solytics.de>
2024-11-08 18:02:06 +0100

    Changed button color

--- src/views/Cta.vue
+++ src/views/Cta.vue
@@ -1,6 +1,6 @@
 <template>
     <a :href="route"
-        class="mt-6 inline-block px-6 py-3 bg-blue-600 text-white font-semibold rounded-md shadow-md hover:bg-blue-700">
+        class="mt-6 inline-block px-6 py-3 bg-red-600 text-white font-semibold rounded-md shadow-md hover:bg-blue-700">
         {{ cta }}
     </a>
 </template>
