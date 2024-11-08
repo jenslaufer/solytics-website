3c381f3f497cc478653fcdcc6b36f98307afcd67
Jens Laufer <jens.laufer@solytics.de>
2024-11-08 13:32:46 +0100

    Added Footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
   <div class="flex flex-col w-screen h-screen font-poppins">
     <router-view class="grow" />
-    <div class="sticky flex bottom-0 gap-4 bg-slate-100 text-slate-600 text-xs italic p-3">
-      <p>&copy; {{ year }} Solytics GmbH</p>
-      <a href="#/impressum" class="hover:text-black">Impressum</a>
-    </div>
+    <app-footer />
   </div>
 </template>
 <script setup>
-import { ref } from 'vue';
-
-const year = ref(new Date().getFullYear());
+import AppFooter from './views/AppFooter.vue';
 </script>
